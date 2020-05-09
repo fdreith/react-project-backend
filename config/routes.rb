f25b1e62323d1e1a36b9873693e66b85060525e9
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-09 15:56:06 -0700

    namespacing for api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  resources :tcomments
-  resources :tasks
-  resources :supervisors
-  resources :employees
-  resources :departments
+
+
+# api is websitedomain.come/api/v1/resource
+  namespace :api do 
+    namespace :v1 do 
+      resources :comments
+      resources :tasks
+      resources :supervisors
+      resources :employees
+      resources :departments
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
