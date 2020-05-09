f6ad0c48fc91ec14a50276f5e4c6e2613a3f9083
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-09 15:56:24 -0700

    adding seeed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# DEPARTMENTS:
+marketing = Department.create(name: "Marketing")
+production = Department.create(name: "Production")
+taproom = Department.create(name: "Taproom")
+
+# SUPERVISORS
+frank = Supervisor.create(name: "Frank", email: "f@f.com", password: "password", department_id: marketing)
+scott = Supervisor.create(name: "Scott", email: "s@s.com", password: "password", department_id: production)
+kipp = Supervisor.create(name: "Kipp", email: "k@k.com", password: "password", department_id: taproom)
+
+# EMPLOYEES
+michelle = Employee.create(name: "Michelle", email: "m@m.com", password: "password", department_id: marketing)
+lauren = Employee.create(name: "Lauren", email: "l@l.com", password: "password", department_id: marketing)
+tara = Employee.create(name: "Tara", email: "t@t.com", password: "password", department_id: marketing)
+
+alex = Employee.create(name: "Alex", email: "a@a.com", password: "password", department_id: production)
+cory = Employee.create(name: "Cory", email: "c@c.com", password: "password", department_id: production)
+ryan = Employee.create(name: "Ryan", email: "r@r.com", password: "password", department_id: production)
+
+ed = Employee.create(name: "Ed", email: "e@e.com", password: "password", department_id: taproom)
+hadley = Employee.create(name: "Hadley", email: "h@h.com", password: "password", department_id: taproom)
+natasha = Employee.create(name: "Natasha", email: "b@b.com", password: "password", department_id: taproom)
+
