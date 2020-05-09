63871a5d6047a76292c6fae71fc46935b23ae949
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-09 16:18:33 -0700

    fixing typo

--- db/migrate/20200509210032_create_comments.rb
+++ db/migrate/20200509210032_create_comments.rb
@@ -1,6 +1,7 @@
-class CreateTcomments < ActiveRecord::Migration[6.0]
+class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
+      t.string :content
       t.belongs_to :task, null: false, foreign_key: true
       t.belongs_to :employee, null: false, foreign_key: true
       t.belongs_to :supervisor, null: false, foreign_key: true
