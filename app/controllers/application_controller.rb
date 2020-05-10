c60d10f249695e6fab95831a909ef55742b15c53
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-09 19:56:08 -0700

    Add mock users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+  helpers do 
+    def current_user
+      Employee.first
+    end
+
+    def logged_in?
+      !!current_user
+    end
+  end
 end
