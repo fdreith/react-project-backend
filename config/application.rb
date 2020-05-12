2f9eab474c827a7a4f3f55911d929db9d3921562
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-11 19:51:29 -0700

    configuring sessions

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,9 @@ module ReactProjectBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use ActionDispatch::Cookies config.middleware.use ActionDispatch::Session::CookieStore, key: '_cookie_name', expire_after: 14. days, httponly:true
+    
   end
 end
 
